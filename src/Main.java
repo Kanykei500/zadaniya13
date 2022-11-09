60493017ae4e9366f40c87626a6f48f0222d2a6e
Kanykei500 <askarbekovakanykei120@gmail.com>
2022-11-09 20:29:38 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Myclass myclass=new Myclass("Kanykei","Askarbekova",19,
+                new String[]{"English,Soft skiil,Practice,Technical"},"Manty");
+        Myclass myclass1=new Myclass("Eliza","Ashyrbaeva",18,
+                new String[]{"\"English,Soft skiil,Practice,Technical"},"Plov");
+
+
+        System.out.println(myclass.getName()+"\n"+myclass.getSurname()+"\n"+myclass.getAge()+"\n"
+                + Arrays.toString(myclass.getLessons()) +"\n"+myclass.getFood());
+        System.out.println(myclass1.getName()+"\n"+myclass1.getSurname()+"\n"+myclass1.getAge()+"\n"
+                + Arrays.toString(myclass1.getLessons()) +"\n"+myclass1.getFood());
+
     }
 }
--- src/Myclass.java
+++ src/Myclass.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Myclass {
+public class Myclass {
+    private String name;
+    private String surname;
+    private int age;
+    private String[] lessons;
+    private String food;
+
+    public Myclass() {
+    }
+
+    public Myclass(String name, String surname, int age, String[] lessons, String food) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.lessons = lessons;
+        this.food = food;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(String[] lessons) {
+        this.lessons = lessons;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
 }
